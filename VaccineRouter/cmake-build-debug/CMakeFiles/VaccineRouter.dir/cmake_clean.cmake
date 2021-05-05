4bcc63f6c1c46024d711a8af4844e8305d682e0a
rodrigotuna <rodrigotuna.andrade@gmail.com>
2021-05-05 09:10:19 +0100

    Fix

---
+++ VaccineRouter/cmake-build-debug/CMakeFiles/VaccineRouter.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VaccineRouter.dir/main.cpp.obj"
+  "VaccineRouter.exe"
+  "VaccineRouter.exe.manifest"
+  "VaccineRouter.pdb"
+  "libVaccineRouter.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VaccineRouter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ VaccineRouter/cmake-build-debug/CMakeFiles/VaccineRouter.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for VaccineRouter.
+# This may be replaced when dependencies are built.
