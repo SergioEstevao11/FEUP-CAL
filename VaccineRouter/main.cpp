4f062108991055c0411586a30ba39836dcd34008
rodrigotuna <rodrigotuna.andrade@gmail.com>
2021-05-10 11:42:39 +0100

    I like it better this way

--- VaccineRouter/main.cpp
+++ VaccineRouter/main.cpp
@@ -7,12 +7,8 @@ int main() {
     Graph g;
     GraphReader gReader(&g);
 
-    std::string nodeS = "..\\maps\\porto_strong_nodes_xy.txt";
-    std::string edgeS = "..\\maps\\porto_strong_edges.txt";
+    std::string nodeS = "../maps/porto_strong_nodes_xy.txt";
+    std::string edgeS = "../maps/porto_strong_edges.txt";
     gReader.readNodes(nodeS);
     gReader.readEdges(edgeS);
-    std::unordered_set<Node*> nd = g.getNodes();
-    for(auto & n : nd){
-        std::cout << n->getId() << std::endl;
-    }
 }
