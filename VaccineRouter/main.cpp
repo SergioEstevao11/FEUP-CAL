7047b86968e41f2329a36fa9620178fa6b94b91f
rodrigotuna <rodrigotuna.andrade@gmail.com>
2021-05-10 16:07:12 +0100

    added new graphs

--- VaccineRouter/main.cpp
+++ VaccineRouter/main.cpp
@@ -10,15 +10,15 @@ int main() {
     Graph g;
     GraphReader gReader(&g);
 
-    std::string nodeS = "../maps/porto_strong_nodes_xy.txt";
-    std::string edgeS = "../maps/porto_strong_edges.txt";
+    std::string nodeS = "../maps/porto_full_nodes_xy.txt";
+    std::string edgeS = "../maps/porto_full_edges.txt";
     gReader.readNodes(nodeS);
     gReader.readEdges(edgeS);
 
     Dijkstra dijk(&g);
     dijk.run(g.getNode(1));
-    std::cout << "unidirected: " << dijk.dist[g.getNode(11)] << std::endl;
+    std::cout << "unidirected: " << dijk.dist[g.getNode(9)] << std::endl;
 
     BiDirectionalDijkstra twodijk(&g);
-    cout << "bidirected: " << twodijk.run(g.getNode(1), g.getNode(11)) << endl;
+    cout << "bidirected: " << twodijk.run(g.getNode(1), g.getNode(9)) << endl;
 }
